a5dc82675da8bf7b081d615f08eb8acce75c1120
AlvaroCascoV <alvarocascovalero@gmail.com>
2024-04-09 20:31:41 +0200

    Empezamos con el index.php

--- TFG/index.php
+++ TFG/index.php
@@ -1,4 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TFG equipo 2</title>
+</head>
+<body>
+    Aquí irá la página
+</body>
+</html>
+
 <?php 
-echo "Hola mundo";
+
 
 ?>
