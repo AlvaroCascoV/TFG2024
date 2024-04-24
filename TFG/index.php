9288975c578546b38d093416f6c176019e07ebee
AlvaroCascoV <alvarocascovalero@gmail.com>
2024-04-24 18:18:40 +0200

    recuperamos indexx

---
+++ TFG/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="index.css" rel="stylesheet">
+    <title>TFG equipo 2</title>
+</head>
+
+<body>
+    <header id="header" class="text-black">
+        <div id="tituloHead">
+            Nombre tienda
+        </div>
+        <div id="botones">
+            <div id="botonInicio">
+                <a href="./index.php"><button class="active"><i class="fas fa-home"></i> Inicio</button></a>
+            </div>
+            <div id="botonDescuentos">
+                <a href="./descuentos.php"><button><i class="fas fa-tag"></i> Descuentos</button></a>
+            </div>
+            <div id="botonDestacados">
+                <a href="./destacados.php"><button> Destacados</button></a>
+            </div>
+            <div id="botonFrutas">
+                <a href=""><button> Frutas</button></a>
+            </div>
+            <div id="botonVerduras">
+                <a href=""><button> Verduras</button></a>
+            </div>
+            <div id="botonVerduras">
+                <a href=""><button> <i class="fas fa-bars"></i></button></a>
+            </div>
+
+        </div>
+    </header>
+
+</body>
+
+</html>
+
+<?php
+echo "prueba php";
+
+?>
