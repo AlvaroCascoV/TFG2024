c9c4ebf858e76d175c55b1ef67fe3a686f7de012
jeliz9 <jenny_ejq@hotmail.com>
2024-05-09 01:16:38 +0200

    verdurasOk

--- TFG/verduras.php
+++ TFG/verduras.php
@@ -14,23 +14,102 @@
 	<h1>Tienda Online</h1>
 
 	<form action="cart.php" method="post">
-		<h2>Pimiento amarillo</h2>
-		<img src="pimientoa.png" alt="Producto 1">
-		<p>Precio: 4.50€/kg </p>
+		<h2>Aguacate Hass</h2>
+		<img src="aguacatehass.jpg" alt="Producto 1">
+		<p>Precio: 1.50€/pieza</p>
 		<label for="cantidad1">Cantidad:</label>
 		<input type="number" id="cantidad1" name="cantidad1" value="0">
 
-		<h2>Pimiento italiano</h2>
-		<img src="pimientoitaliano.png" alt="Producto 2">
-		<p>Precio: 3€ /kg</p>
+		<h2>Berenjena</h2>
+		<img src="berenjena.jpg" alt="Producto 2">
+		<p>Precio: 4.50€/kg </p>
 		<label for="cantidad2">Cantidad:</label>
 		<input type="number" id="cantidad2" name="cantidad2" value="0">
-       
-        <h2>Repollo</h2>
-		<img src="repollo.png" alt="Producto 3">
-		<p>Precio: 3.50€/pieza</p>
+
+		<h2>Boniato</h2>
+		<img src="boniato.jpg" alt="Producto 3">
+		<p>Precio: 2.50€/kg </p>
 		<label for="cantidad3">Cantidad:</label>
-		<input type="number" id="cantidad3" name="cantidad3" value="0">
+		<input type="number" id="cantidad1"3name="cantidad3" value="0">
+
+		<h2>Brocoli</h2>
+		<img src="brocoli.jpg" alt="Producto 4">
+		<p>Precio: 3.50€/pieza</p>
+		<label for="cantidad4">Cantidad:</label>
+		<input type="number" id="cantidad4" name="cantidad4" value="0">
+
+		<h2>Calabacin</h2>
+		<img src="calabacin.jpg" alt="Producto 5">
+		<p>Precio: 3.50€/kg</p>
+		<label for="cantidad5">Cantidad:</label>
+		<input type="number" id="cantidad5" name="cantidad5" value="0">
+
+		<h2>Calabaza cacahuete</h2>
+		<img src="calabaza-cacahuete.jpg" alt="Producto 6">
+		<p>Precio: 3.50€/pieza</p>
+		<label for="cantidad6">Cantidad:</label>
+		<input type="number" id="cantidad6" name="cantidad6" value="0">
+
+		<h2>Cebolla</h2>
+		<img src="cebolla.jpg" alt="Producto 7">
+		<p>Precio: 3.50€/kg</p>
+		<label for="cantidad7">Cantidad:</label>
+		<input type="number" id="cantidad7" name="cantidad7" value="0">
+
+		<h2>Curcuma fresca</h2>
+		<img src="curcuma.jpg" alt="Producto 8">
+		<p>Precio: 2.50€/ pack</p>
+		<label for="cantidad8">Cantidad:</label>
+		<input type="number" id="cantidad8" name="cantidad8" value="0">
+
+		<h2>Jengibre</h2>
+		<img src="jengibre.jpg" alt="Producto 9">
+		<p>Precio: 6€ /kg</p>
+		<label for="cantidad9">Cantidad:</label>
+		<input type="number" id="cantidad9" name="cantidad9" value="0">
+       
+		<h2>Kale</h2>
+		<img src="kale.jpg" alt="Producto 10">
+		<p>Precio: 3.50€/pack</p>
+		<label for="cantidad10">Cantidad:</label>
+		<input type="number" id="cantidad10" name="cantidad10" value="0">
+		
+		<h2>Pimiento amarillo</h2>
+		<img src="pimientoa.png" alt="Producto 11">
+		<p>Precio: 4.50€/kg </p>
+		<label for="cantidad11">Cantidad:</label>
+		<input type="number" id="cantidad11" name="cantidad11" value="0">
+
+		
+		<h2>Pimiento italiano</h2>
+		<img src="pimientoitaliano.jpg" alt="Producto 12">
+		<p>Precio: 3.50€/pieza</p>
+		<label for="cantidad12">Cantidad:</label>
+		<input type="number" id="cantidad12" name="cantidad12" value="0">
+
+		<h2>Pimiento tricolor</h2>
+		<img src="pimientoTricolor.jpeg" alt="Producto 13">
+		<p>Precio: 3.50€/pack</p>
+		<label for="cantidad13">Cantidad:</label>
+		<input type="number" id="cantidad13" name="cantidad13" value="0">
+		
+		<h2>Puerro</h2>
+		<img src="puerro.jpg" alt="Producto 14">
+		<p>Precio: 3.50€/pack</p>
+		<label for="cantidad14">Cantidad:</label>
+		<input type="number" id="cantidad14" name="cantidad14" value="0">
+
+		<h2>Tomate rama</h2>
+		<img src="tomaterama.jpg" alt="Producto 15">
+		<p>Precio: 3.00€/kg</p>
+		<label for="cantidad15">Cantidad:</label>
+		<input type="number" id="cantidad15" name="cantidad15" value="0">
+
+		<h2>Zaanahoria</h2>
+		<img src="zanahoria.jpg" alt="Producto 16">
+		<p>Precio: 2.50€/kg</p>
+		<label for="cantidad16">Cantidad:</label>
+		<input type="number" id="cantidad16" name="cantidad16" value="0">
 		
         <button type="submit">Agregar al carrito</button>
 	</form>
