684c05634b53634eab4cb1f50d08294a87e9c7ee
jeliz9 <jenny_ejq@hotmail.com>
2024-06-03 14:09:51 +0200

    clasificacion FyV Ok

--- TFG/frutas.php
+++ TFG/frutas.php
@@ -27,73 +27,89 @@
 	<form action="cart.php" method="post">
 		<div>
 			<h2>Cereza</h2>
-			<div class="foto_carrito"><img src="assets/images/frutas/cherry.png" alt="foto de cerezas"></div>
+			<div class="foto_carrito"><img src="assets/images/frutas/cherry.png" alt="Foto de cerezas"></div>
 			<p>Precio: 7.50€ / kg </p>
 			<label for="cantidad30">Cantidad:</label>
 			<input type="number" id="cantidad30" name="cantidad30" value="0" min="0">
 		</div>
 
-		<h2>Ciruela roja</h2>
-		<img src="ciruela.png" alt="hola 31">
-		<p>Precio: 4.50€ / kg </p>
-		<label for="cantidad31">Cantidad:</label>
-		<input type="number" id="cantidad31" name="cantidad30" value="0" min="0">
+        <div> 
+			<h2>Ciruela roja</h2>
+			<div class="foto carrito"> <img src="assets/images/frutas/ciruela.png" alt="Foto de ciruela"></div>
+			<p>Precio: 4.50€ / kg </p>
+			<label for="cantidad31">Cantidad:</label>
+			<input type="number" id="cantidad31" name="cantidad30" value="0" min="0">
+		</div>
 
-		<h2>Frambuesa</h2>
-		<img src="frambuesa.png" alt="Producto 32">
-		<p>Precio: 3.50€ / bandeja </p>
-		<label for="cantidad32">Cantidad:</label>
-		<input type="number" id="cantidad32" name="cantidad32" value="0" min="0">
+		<div>
+			<h2>Frambuesa</h2>
+			<div class="foto carrito"><img src="assets/images/frutas/frambuesa.png" alt="Foto de frambuesa"></div>
+			<p>Precio: 3.50€ / bandeja </p>
+			<label for="cantidad32">Cantidad:</label>
+			<input type="number" id="cantidad32" name="cantidad32" value="0" min="0">
+		</div>
 
-		<h2>Kiwi verde</h2>
-		<img src="kiwi.png" alt="Producto 33">
-		<p>Precio: 5.50€ / kg </p>
-		<label for="cantidad33">Cantidad:</label>
-		<input type="number" id="cantidad33" name="cantidad33" value="0" min="0">
+		<div>
+			<h2>Kiwi verde</h2>
+			<div class="foto carrito"><img src="assets/images/frutas/kiwi.png" alt="Foto de kiwi"></div>
+			<p>Precio: 5.50€ / kg </p>
+			<label for="cantidad33">Cantidad:</label>
+			<input type="number" id="cantidad33" name="cantidad33" value="0" min="0">
+		</div>
 
-		<h2>Lima</h2>
-		<img src="lima.png" alt="Producto 34">
-		<p>Precio: 6.50€ / kg</p>
-		<label for="cantidad34">Cantidad:</label>
-		<input type="number" id="cantidad34" name="cantidad34" value="0" min="0">
+		<div>
+			<h2>Lima</h2>
+			<div class="foto carrito"><img src="assets/images/frutas/lima.png" alt="Foto de lima"></div>
+			<p>Precio: 6.50€ / kg</p>
+			<label for="cantidad34">Cantidad:</label>
+			<input type="number" id="cantidad34" name="cantidad34" value="0" min="0">
+		</div>
 
-		<h2>Limón</h2>
-		<img src="limon.png" alt="Producto 35">
-		<p>Precio: 3.50€ / kg </p>
-		<label for="cantidad35">Cantidad:</label>
-		<input type="number" id="cantidad35" name="cantidad35" value="0" min="0">
+		<div>
+			<h2>Limón</h2>
+			<div class="foto carrito"><img src="assets/images/frutas/limon.png" alt="Foto de limón"></div>
+			<p>Precio: 3.50€ / kg </p>
+			<label for="cantidad35">Cantidad:</label>
+			<input type="number" id="cantidad35" name="cantidad35" value="0" min="0">
+		</div>
 
-		<h2>Mandarina</h2>
-		<img src="mandarina.png" alt="Producto 36">
-		<p>Precio: 3.50€ / kg</p>
-		<label for="cantidad36">Cantidad:</label>
-		<input type="number" id="cantidad36" name="cantidad36" value="0" min="0">
+		<div>
+			<h2>Mandarina</h2>
+			<div class="foto carrito"><img src="assets/images/frutas/mandarina.png" alt="Foto de mandarina"></div>
+			<p>Precio: 3.50€ / kg</p>
+			<label for="cantidad36">Cantidad:</label>
+			<input type="number" id="cantidad36" name="cantidad36" value="0" min="0">
+		</div>
+		<div>
+			<h2>Manzana golden</h2>
+			<div class="foto carrito"><img src="assets/images/frutas/manzana-golden.png" alt="Foto de manzana golden"> </div>
+			<p>Precio: 4.50€ / kg</p>
+			<label for="cantidad37">Cantidad:</label>
+			<input type="number" id="cantidad37" name="cantidad37" value="0" min="0">
+		</div>
+		<div>
+			<h2>Manzana golden</h2>
+			<div class="foto carrito"><img src="assets/images/frutas/manzana-story.png" alt="Foto de manzana story"> </div>
+			<p>Precio: 3.50€ / kg</p>
+			<label for="cantidad38">Cantidad:</label>
+			<input type="number" id="cantidad38" name="cantidad38" value="0" min="0">
+		</div>
 
-		<h2>Manzana golden</h2>
-		<img src="manzana-golden.png" alt="Producto 37">
-		<p>Precio: 4.50€ / kg</p>
-		<label for="cantidad37">Cantidad:</label>
-		<input type="number" id="cantidad37" name="cantidad37" value="0" min="0">
+		<div>
+			<h2>Naranja de mesa</h2>
+			<div class="foto carrito"><img src="assets/images/frutas/naranja.png" alt="Foto de naranja de mesa"> </div>
+			<p>Precio: 3.50€ / kg</p>
+			<label for="cantidad39">Cantidad:</label>
+			<input type="number" id="cantidad39" name="cantidad39" value="0" min="0">
+		</div>
 
-		<h2>Manzana golden</h2>
-		<img src="manzana-story.png" alt="Producto 38">
-		<p>Precio: 3.50€ / kg</p>
-		<label for="cantidad38">Cantidad:</label>
-		<input type="number" id="cantidad38" name="cantidad38" value="0" min="0">
-
-
-		<h2>Naranja de mesa</h2>
-		<img src="naranja-mesa.png" alt="Producto 39">
-		<p>Precio: 3.50€ / kg</p>
-		<label for="cantidad39">Cantidad:</label>
-		<input type="number" id="cantidad39" name="cantidad39" value="0" min="0">
-
-		<h2>Plátano de canarias</h2>
-		<img src="platano-de-canarias.png" alt="Producto 40">
-		<p>Precio: 3.50€ / kg</p>
-		<label for="cantidad40">Cantidad:</label>
-		<input type="number" id="cantidad40" name="cantidad40" value="0" min="0">
-
+		<div>
+			<h2>Plátano de canarias</h2>
+			<div class="foto carrito"><img src="assets/images/frutas/platano-de-canarias.png" alt="Foto de plátano de Canarias"> </div>
+			<p>Precio: 3.50€ / kg</p>
+			<label for="cantidad40">Cantidad:</label>
+			<input type="number" id="cantidad40" name="cantidad40" value="0" min="0">
+		</div>
 
 		<button type="submit">Agregar al carrito</button>
 	</form>
--- TFG/verduras.php
+++ TFG/verduras.php
@@ -23,103 +23,134 @@ include("header.php");
 	<h1>Tienda Online</h1>
 
 	<form action="cart.php" method="post">
-		<h2>Aguacate Hass</h2>
-		<div><img class="foto_carrito" src="assets/images/aguacatehass.png" alt="Producto 1"></div>
-		<p>Precio: 1.50€ / pieza</p>
-		<label for="cantidad1">Cantidad:</label>
-		<input type="number" id="cantidad1" name="cantidad1" value="0" min="0">
+		<div>
+			<h2>Aguacate Hass</h2>
+			<div><img class="foto_carrito" src="assets/images/verduras/aguacatehass.png" alt="foto de aguacate"></div>
+			<p>Precio: 1.50€ / pieza</p>
+			<label for="cantidad1">Cantidad:</label>
+			<input type="number" id="cantidad1" name="cantidad1" value="0" min="0">
+		</div>
 
-		<h2>Berenjena</h2>
-		<img src="berenjena.png" alt="Producto 2">
-		<p>Precio: 4.50€ / kg </p>
-		<label for="cantidad2">Cantidad:</label>
-		<input type="number" id="cantidad2" name="cantidad2" value="0" min="0">
+		<div>
+			<h2>Berenjena</h2>
+			<div><img class="foto_carrito" src="assets/images/verduras/berenjena.png" alt="Foto de berenjena"></div>
+			<p>Precio: 4.50€ / kg </p>
+			<label for="cantidad2">Cantidad:</label>
+			<input type="number" id="cantidad2" name="cantidad2" value="0" min="0">
+		</div>
 
-		<h2>Boniato</h2>
-		<img src="boniato.png" alt="Producto 3">
-		<p>Precio: 2.50€ / kg </p>
-		<label for="cantidad3">Cantidad:</label>
-		<input type="number" id="cantidad1"3name="cantidad3" value="0" min="0">
+		<div>
+			<h2>Boniato</h2>
+			<div><img class="foto_carrito" src="assets/images/verduras/boniato.png" alt="Foto de boniato"></div>
+			<p>Precio: 2.50€ / kg </p>
+			<label for="cantidad3">Cantidad:</label>
+			<input type="number" id="cantidad1"3name="cantidad3" value="0" min="0">
+		</div>
 
-		<h2>Brocoli</h2>
-		<img src="brocoli.png" alt="Producto 4">
-		<p>Precio: 3.50€ / pieza</p>
-		<label for="cantidad4">Cantidad:</label>
-		<input type="number" id="cantidad4" name="cantidad4" value="0" min="0">
+		<div>
+			<h2>Brocoli</h2>
+			<div><img class="foto_carrito" src="assets/images/verduras/brocoli.png" alt="Foto de brocoli"></div>
+			<p>Precio: 3.50€ / pieza</p>
+			<label for="cantidad4">Cantidad:</label>
+			<input type="number" id="cantidad4" name="cantidad4" value="0" min="0">
+		</div>
 
-		<h2>Calabacin</h2>
-		<img src="calabacin.png" alt="Producto 5">
-		<p>Precio: 3.50€ / kg</p>
-		<label for="cantidad5">Cantidad:</label>
-		<input type="number" id="cantidad5" name="cantidad5" value="0" min="0">
+		<div>
+			<h2>Calabacin</h2>
+			<div><img class="foto_carrito" src="assets/images/verduras/calabacin.png" alt="Foto de calabacín"></div>
+			<p>Precio: 3.50€ / kg</p>
+			<label for="cantidad5">Cantidad:</label>
+			<input type="number" id="cantidad5" name="cantidad5" value="0" min="0">
+		</div>
 
-		<h2>Calabaza cacahuete</h2>
-		<img src="calabaza-cacahuete.png" alt="Producto 6">
-		<p>Precio: 3.50€ / pieza</p>
-		<label for="cantidad6">Cantidad:</label>
-		<input type="number" id="cantidad6" name="cantidad6" value="0" min="0">
+		<div>
+			<h2>Calabaza cacahuete</h2>
+			<div><img class="foto_carrito" src="assets/images/verduras/calabaza-cacahuete.png" alt="Foto de calabaza"></div>
+			<p>Precio: 3.50€ / pieza</p>
+			<label for="cantidad6">Cantidad:</label>
+			<input type="number" id="cantidad6" name="cantidad6" value="0" min="0">
+		</div>
 
-		<h2>Cebolla</h2>
-		<img src="cebolla.png" alt="Producto 7">
-		<p>Precio: 3.50€ / kg</p>
-		<label for="cantidad7">Cantidad:</label>
-		<input type="number" id="cantidad7" name="cantidad7" value="0" min="0">
-
-		<h2>Curcuma fresca</h2>
-		<img src="curcuma.png" alt="Producto 8">
-		<p>Precio: 2.50€ / pack</p>
-		<label for="cantidad8">Cantidad:</label>
-		<input type="number" id="cantidad8" name="cantidad8" value="0" min="0">
-
-		<h2>Jengibre</h2>
-		<img src="jengibre.png" alt="Producto 9">
-		<p>Precio: 6€ / kg</p>
-		<label for="cantidad9">Cantidad:</label>
-		<input type="number" id="cantidad9" name="cantidad9" value="0" min="0">
-       
-		<h2>Kale</h2>
-		<img src="kale.png" alt="Producto 10">
-		<p>Precio: 3.50€ / pack</p>
-		<label for="cantidad10">Cantidad:</label>
-		<input type="number" id="cantidad10" name="cantidad10" value="0" min="0">
+		<div>
+			<h2>Cebolla</h2>
+			<div><img class="foto_carrito" src="assets/images/verduras/cebolla.png" alt="Foto de cebolla"></div>
+			<p>Precio: 3.50€ / kg</p>
+			<label for="cantidad7">Cantidad:</label>
+			<input type="number" id="cantidad7" name="cantidad7" value="0" min="0">
+		</div>
 		
-		<h2>Pimiento amarillo</h2>
-		<img src="pimientoa.png" alt="Producto 11">
-		<p>Precio: 4.50€ / kg </p>
-		<label for="cantidad11">Cantidad:</label>
-		<input type="number" id="cantidad11" name="cantidad11" value="0" min="0">
+		<div>
+			<h2>Curcuma fresca</h2>
+			<div><img class="foto_carrito" src="assets/images/verduras/curcuma.png" alt="Foto de curcuma"></div>
+			<p>Precio: 2.50€ / pack</p>
+			<label for="cantidad8">Cantidad:</label>
+			<input type="number" id="cantidad8" name="cantidad8" value="0" min="0">
+		</div>
 
+		<div>
+			<h2>Jengibre</h2>
+			<div><img class="foto_carrito" src="assets/images/verduras/jengibre.png" alt="Foto de jengibre"></div>
+			<p>Precio: 6€ / kg</p>
+			<label for="cantidad9">Cantidad:</label>
+			<input type="number" id="cantidad9" name="cantidad9" value="0" min="0">
+		</div>
+
+		<div>
+			<h2>Kale</h2>
+			<div><img class="foto_carrito" src="assets/images/verduras/kale.png" alt="Foto de kale"></div>
+			<p>Precio: 3.50€ / pack</p>
+			<label for="cantidad10">Cantidad:</label>
+			<input type="number" id="cantidad10" name="cantidad10" value="0" min="0">
+		</div>
 		
-		<h2>Pimiento italiano</h2>
-		<img src="pimiento-italiano.png" alt="Producto 12">
-		<p>Precio: 3.50€ / pieza</p>
-		<label for="cantidad12">Cantidad:</label>
-		<input type="number" id="cantidad12" name="cantidad12" value="0" min="0">
+		<div>
+			<h2>Pimiento amarillo</h2>
+			<div><img class="foto_carrito" src="assets/images/verduras/pimientoa.png" alt="Foto de pimiento amamrillo"></div>
+			<p>Precio: 4.50€ / kg </p>
+			<label for="cantidad11">Cantidad:</label>
+			<input type="number" id="cantidad11" name="cantidad11" value="0" min="0">
+		</div>
+			
+		<div>
+			<h2>Pimiento italiano</h2>
+			<div><img class="foto_carrito" src="assets/images/verduras/pimiento-italiano.png" alt="Foto de pimiento italiano"></div>
+			<p>Precio: 3.50€ / pieza</p>
+			<label for="cantidad12">Cantidad:</label>
+			<input type="number" id="cantidad12" name="cantidad12" value="0" min="0">
+		</div>
 
-		<h2>Pimiento tricolor</h2>
-		<img src="pimientoTricolor.png" alt="Producto 13">
-		<p>Precio: 3.50€ / pack</p>
-		<label for="cantidad13">Cantidad:</label>
-		<input type="number" id="cantidad13" name="cantidad13" value="0" min="0">
-		
-		<h2>Puerro</h2>
-		<img src="puerro.png" alt="Producto 14">
-		<p>Precio: 3.50€ / pack</p>
-		<label for="cantidad14">Cantidad:</label>
-		<input type="number" id="cantidad14" name="cantidad14" value="0" min="0">
+		<div>
+			<h2>Pimiento tricolor</h2>
+			<div><img class="foto_carrito" src="assets/images/verduras/pimientoTricolor.png" alt="Foto de pimiento tricolor"></div>
+			<p>Precio: 3.50€ / pack</p>
+			<label for="cantidad13">Cantidad:</label>
+			<input type="number" id="cantidad13" name="cantidad13" value="0" min="0">
+			</div>
 
-		<h2>Tomate rama</h2>
-		<img src="tomate-rama.png" alt="Producto 15">
-		<p>Precio: 3.00€ / kg</p>
-		<label for="cantidad15">Cantidad:</label>
-		<input type="number" id="cantidad15" name="cantidad15" value="0" min="0">
+		<div>
+			<h2>Puerro</h2>
+			<div><img class="foto_carrito" src="assets/images/verduras/puerro.png" alt="Foto de puerro"></div>
+			<p>Precio: 3.50€ / pack</p>
+			<label for="cantidad14">Cantidad:</label>
+			<input type="number" id="cantidad14" name="cantidad14" value="0" min="0">
+		</div>
 
-		<h2>Zanahoria</h2>
-		<img src="zanahoria.png" alt="Producto 16">
-		<p>Precio: 2.50€ / kg</p>
-		<label for="cantidad16">Cantidad:</label>
-		<input type="number" id="cantidad16" name="cantidad16" value="0" min="0">
-		
+		<div>
+			<h2>Tomate rama</h2>
+			<div><img class="foto_carrito" src="assets/images/verduras/tomate-rama.png" alt="Foto de tomate rama"></div>
+			<p>Precio: 3.00€ / kg</p>
+			<label for="cantidad15">Cantidad:</label>
+			<input type="number" id="cantidad15" name="cantidad15" value="0" min="0">
+		</div>
+
+		<div>
+			<h2>Zanahoria</h2>
+			<div><img class="foto_carrito" src="assets/images/verduras/zanahoria.png" alt="Foto de zanahoria"></div>
+			<p>Precio: 2.50€ / kg</p>
+			<label for="cantidad16">Cantidad:</label>
+			<input type="number" id="cantidad16" name="cantidad16" value="0" min="0">
+		</div>	
+
         <button type="submit">Agregar al carrito</button>
 	</form>
 </body>
